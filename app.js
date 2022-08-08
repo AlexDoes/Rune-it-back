3263a536067fb3e520394b5694e4190a2d4a10a0
AlexDoes <alexsw.191@gmail.com>
2022-08-08 17:16:33 -0400

    keys ignore

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors');
+const mongoose = require('mongoose')
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get(`/`,(request,response) => {
+    response.send(`<img width=100% height=100%
+    src=https://i.kym-cdn.com/entries/icons/original/000/033/108/Screen_Shot_2020-03-06_at_3.25.32_PM.jpg
+    >`);
+})
+
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`);
+});
+
