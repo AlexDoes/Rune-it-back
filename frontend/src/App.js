dc91eacaccf5468d83e1b02aec611ecfd9e71ec0
AlexDoes <alexsw.191@gmail.com>
2022-08-09 16:45:33 -0400

    store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,12 @@
+import MainPage from './components/main/main_page';
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
+var link = 'https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Dark Magician'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <MainPage/>
+    )
 }
 
 export default App;
---
+++ frontend/src/components/main/main_page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainPage(props){
+    return(
+    <>
+    dbsigindsifnins
+    </>
+    )
+}
+
+export default MainPage
