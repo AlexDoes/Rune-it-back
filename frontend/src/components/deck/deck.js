a086ef756f509792d14ec9b4c0b5ecc7530d2862
AlexDoes <alexsw.191@gmail.com>
2022-08-09 20:22:52 -0400

    fixed the data

--- frontend/src/components/deck/deck.js
+++ frontend/src/components/deck/deck.js
@@ -13,26 +13,16 @@ function Deck() {
         })
     },[]) // [] == useEffect only when it mounts (commponent did mount)
     
-    // const checkDeck = () => {
-    //     if (deck.card_images !== undefined) {
-    //         return deck.card_images[0].image_url
-    //     } else {
-    //         return ''
-    //     }
-    // }
-    
-    console.log(deck)
     const checkDeck = deck.card_images ? deck.card_images[0].image_url : ''
     const checkPrices = deck.card_prices ? deck.card_prices[0] : []
     const checkSets = deck.card_sets ? deck.card_sets : []
-    // const prices = checkPrices.map(price => <li>price</li>)
     const prices = Object.entries(checkPrices).map((k,v) => <li key={v}>{k[0].split("_")[0]}: ${k[1]}</li>)
     const sets = Object.entries(checkSets).map((k,v) => Object.entries(k[1])).map((a,i) => <div key={i}> 
         <b>Name</b>: {a[0][1]} 
         <b>Code:</b> {a[1][1]} 
-        <b>Price:</b> {a[1][1]} 
-        <b>Rarity:</b> {a[1][1]} 
-        <b>Rarity Code:</b> {a[1][1]} 
+        <b>Price:</b> {a[2][1]} 
+        <b>Rarity:</b> {a[3][1]} 
+        <b>Rarity Code:</b> {a[4][1]} 
         </div>
     )
     return (
