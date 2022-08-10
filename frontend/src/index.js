0fa3d7f103781de9959d9d8f6b52a444a659e06b
AlexDoes <alexsw.191@gmail.com>
2022-08-09 20:04:46 -0400

    imgs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,9 +8,9 @@ import {configureStore} from '@reduxjs/toolkit'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Root/>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 // ReactDOM.render(<Root store={store} />, root);
 
