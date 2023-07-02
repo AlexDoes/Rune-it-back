a54a6373f6cd44a42a382985b195c3213ef4daa2
AlexDoes <alexsw.191@gmail.com>
2023-07-02 17:33:23 -0400

    parser

--- frontend/src/components/ScrapedResults.jsx
+++ frontend/src/components/ScrapedResults.jsx
@@ -2,21 +2,56 @@ import * as fs from "fs";
 import axios from "axios";
 import * as cheerio from "cheerio";
 import { useState, useEffect } from "react";
+import DOMPurify from "dompurify";
+import parse from "html-react-parser";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import SyntaxHighlighter from "react-syntax-highlighter";
+// import { duoToneForest } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import {
+  duotoneDark,
+  duotoneForest,
+} from "react-syntax-highlighter/dist/esm/styles/prism";
+
 export default function ScrapedResults(props) {
   console.log(props);
   console.log(props.searchQuest);
   const [quest, setQuest] = useState(props.searchQuest || "Regicide");
-  const [renderHtml, setRenderHtml] = useState("");
+  const [renderHtml, setRenderHtml] = useState([]);
 
   useEffect(() => {
     const data = scrapeWiki(String(quest));
     data.then((result) => {
-      console.log(result);
       setRenderHtml(result);
     });
   }, [quest]);
 
-  return <>{renderHtml}</>;
+  const rewardsDisplay = () => {
+    if (renderHtml.length === 0) {
+      return <div></div>;
+    }
+    let sanitizedHtml = DOMPurify.sanitize(renderHtml[1]);
+    let sanitizedImg = DOMPurify.sanitize(renderHtml[0]);
+    console.log(sanitizedImg);
+    return (
+      <div className="flex flex-col">
+        HTML for rewards image from OSRS Wiki:
+        <SyntaxHighlighter
+          language="html"
+          style={duotoneForest}
+          wrapLongLines
+          wrapLines
+        >
+          {sanitizedImg}
+        </SyntaxHighlighter>
+        HTML for rewards from OSRS Wiki:
+        <SyntaxHighlighter language="html" style={duotoneDark} wrapLines>
+          {sanitizedHtml}
+        </SyntaxHighlighter>
+      </div>
+    );
+  };
+
+  return <>{rewardsDisplay()}</>;
 }
 
 async function scrapeWiki(quest) {
@@ -38,7 +73,10 @@ async function scrapeWiki(quest) {
       const ulElement = ulElement.next();
     }
     const rewards = [];
-    console.log(rewards);
+    console.log(ulElement.html());
+    console.log(rewardImg.html());
+    rewards.push(rewardImg.html());
+    rewards.push(ulElement.html());
     return rewards;
   } catch (error) {
     console.log(error);
