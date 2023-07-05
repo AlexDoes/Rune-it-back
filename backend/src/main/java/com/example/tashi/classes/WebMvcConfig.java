66393bd8f19018ee4743c8221a80a137034279a7
StevenSookhai <StevenSookhai@gmail.com>
2023-07-05 04:33:10 -0400

    preparing for hosting

--- backend/src/main/java/com/example/tashi/classes/WebMvcConfig.java
+++ backend/src/main/java/com/example/tashi/classes/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173","https://rune-it-back-l4p0.onrender.com")
                 .allowedMethods("*")
                 .allowCredentials(true);
     }
